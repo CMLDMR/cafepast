2105e0c91bcf1bba99a5d04a827ce908ffd53b09
Cemal Demir <CMLDMR@users.noreply.github.com>
2023-04-11 13:12:16 +0300

    Mongo Driver Added

---
+++ cafepast/cafecore/kategoriitem.cpp
@@ -0,0 +1,16 @@
+
+#include "kategoriitem.h"
+
+namespace Cafe {
+namespace Kategori {
+
+KategoriItem::KategoriItem()
+    :MongoCore::Item(Key::Collection.data())
+{
+
+}
+
+}
+
+} // namespace Cafe
+
